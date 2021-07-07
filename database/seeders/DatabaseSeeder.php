6f1c9735a88af4238723e97ecd90c4c8622328cd
nataliavarela24 <natalianicole.varela@gmail.com>
2021-07-06 23:18:26 -0400

    add migratiion ciudad

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RegionSeeder::class);
         $this->call(ProvinciaSeeder::class);
+       
+    
     }
 }
